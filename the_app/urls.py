6901a455c2508338302240dba62fd699fdbc81ba
mouad <m.harmach@gmail.com>
2023-07-20 16:46:42 +0200

    add: the_app/template & setting.py:the_app.apps.TheAppConfig

--- the_app/urls.py
+++ the_app/urls.py
@@ -19,7 +19,8 @@ from django.urls import path
 from the_app import views
 
 urlpatterns = [
-    #path('', views.hello),
+    # path('', views.hello),
     path('job/<int:id>', views.job_details, name='job_details'),
     path('', views.jobs, name='jobs_home'),
+    path('hello', views.hello, name='hello'),
 ]
--- the_app/views.py
+++ the_app/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
+# from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 from django.urls import reverse
+from django.template import loader
 
 job_titles = [
         "job1 title",
@@ -13,24 +15,29 @@ job_descs = [
         "job3 dessssk"
         ]
 
+
 # Create your views here.
 def hello(req):
-    return HttpResponse("hello tdchjankgow")
+    template = loader.get_template("hello.html")
+    ctx = {}
+    return HttpResponse(template.render(ctx, req))
+
 
 def jobs(req):
     html = "<ul>"
     for title in job_titles:
         job_id = job_titles.index(title)
         url = reverse('job_details', args=(job_id,))
-        #html += f"<li><a href='job/{job_id}'>{title}</a></li>"
+        # html += f"<li><a href='job/{job_id}'>{title}</a></li>"
         html += f"<li><a href='{url}'>{title}</a></li>"
     html += "</ul>"
     return HttpResponse(html)
 
+
 def job_details(req, id):
     try:
         if id == 100:
-            #return redirect("/jobs")
+            # eturn redirect("/jobs")
             return redirect(reverse('jobs_home'))
         html_return = f"<h1>title: {job_titles[id]}</h1>desc: {job_descs[id] }<h3></h3>"
         return HttpResponse(html_return)
