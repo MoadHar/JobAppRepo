d7f59df36f352c82143f322e80951d761d424282
mouad <m.harmach@gmail.com>
2023-07-23 15:57:17 +0200

    change joblist and job details to templates

--- the_app/views.py
+++ the_app/views.py
@@ -4,11 +4,13 @@ from django.http import HttpResponse, HttpResponseNotFound
 from django.urls import reverse
 # from django.template import loader
 
+
 job_titles = [
         "job1 title",
         "job2 title",
         "job3 title",
         ]
+
 job_descs = [
         "job1 decription",
         "job2 desc",
@@ -29,12 +31,16 @@ def hello(req):
     # template = loader.get_template("hello.html")
     # template = loader.get_template("the_app/hello.html")
     temp_obj = TempClass
-    ctx = {"name": "Djangow", "first_list": tmp_list, "temp_obj": temp_obj, "age":44 }
-    #return HttpResponse(template.render(ctx, req))
+    is_authenticated = True
+    ctx = {"name": "Djangow", "first_list": tmp_list, "temp_obj": temp_obj, "age":44,
+           "is_authed": is_authenticated
+           }
+    # return HttpResponse(template.render(ctx, req))
     return render(req, "the_app/hello.html", ctx)
 
 
 def jobs(req):
+    """
     html = "<ul>"
     for title in job_titles:
         job_id = job_titles.index(title)
@@ -43,6 +49,9 @@ def jobs(req):
         html += f"<li><a href='{url}'>{title}</a></li>"
     html += "</ul>"
     return HttpResponse(html)
+    """
+    ctx = {"job_titles": job_titles, "count": len(job_titles)}
+    return render(req, 'the_app/jobs-list.html', ctx)
 
 
 def job_details(req, id):
@@ -50,7 +59,14 @@ def job_details(req, id):
         if id == 100:
             # eturn redirect("/jobs")
             return redirect(reverse('jobs_home'))
-        html_return = f"<h1>title: {job_titles[id]}</h1>desc: {job_descs[id] }<h3></h3>"
-        return HttpResponse(html_return)
+        # html_return = f"<h1>title: {job_titles[id]}</h1> <h3> desc: {job_descs[id]}</h3>"
+        # return HttpResponse(html_return)
+        ctx = {
+        "job_title": job_titles[id],
+        "job_desc": job_descs[id],
+        }
+        return render(req, 'the_app/job-details.html', ctx)
     except:
         return HttpResponseNotFound("Not found")
+    
+    
