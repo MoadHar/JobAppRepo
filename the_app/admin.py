0551deda9a680d1613f8776a2112a19901276ac5
mouad <m.harmach@gmail.com>
2023-07-24 17:45:56 +0200

    finishing admin customisation

--- the_app/admin.py
+++ the_app/admin.py
@@ -5,7 +5,23 @@ from .models import Person, JobPost
 
 class JobAdmin(admin.ModelAdmin):
     list_display=('title', 'expiry', 'salary')
+    list_filter=('expiry','salary')
+    search_fields=('title', 'description')
+    search_help_text = "search in title or/and description fields"
 
+    # ------- now detail view
+    # fields = (("title", "description"), "salary") //or fieldset bellow, not both
+    # exclude = ("expiry")
+
+    fieldsets = (
+            ('Basic Information', {
+                'fields':('title', 'description')}
+             ),
+            ('More Information', {
+                'classes':('collapse','wide'), # adding builting css stylings
+                'fields':(('expiry', 'salary'), 'slug')}
+             ),
+    )
 # Register your models here.
 admin.site.register(JobPost, JobAdmin)
 admin.site.register(Person)
