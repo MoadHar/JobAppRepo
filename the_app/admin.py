a55df55ee299527c788c2ec7aa0270538f1f4f81
mouad <m.harmach@gmail.com>
2023-07-24 16:54:24 +0200

    customizing list pannel displayed fields

--- the_app/admin.py
+++ the_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Person, JobPost
+
+
+class JobAdmin(admin.ModelAdmin):
+    list_display=('title', 'expiry', 'salary')
+
 # Register your models here.
+admin.site.register(JobPost, JobAdmin)
+admin.site.register(Person)
