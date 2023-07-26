96f8b262cd24399b814711eeec2297c4b8e10a4b
mouad <m.harmach@gmail.com>
2023-07-26 23:59:03 +0200

    adding subs to database

--- subscribe/views.py
+++ subscribe/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 
+from subscribe.models import Subscribe
+
 # Create your views here.
 def subscribe(req):
 	ctx = {}
+	if req.POST:
+		fname = req.POST["firstname"]
+		lname = req.POST["lastname"]
+		email = req.POST["email"]
+		print("this is a POST method request:", fname, lname, email)
+		if email =="" or fname == "" or lname =="":
+			ctx["email_empty"] = "email or fname or lname not entered"
+		else:
+			subsribe = Subscribe(first_name=fname, last_name=lname, email=email)
+			subsribe.save()
+
 	return render(req, 'subscribe/subscribe.html', ctx)
